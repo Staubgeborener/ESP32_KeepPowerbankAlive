3b93371045621c9e9a219f6dda75e98b273466b1
Staubgeborener <ericschroeder91@web.de>
2017-07-21 15:09:52 +0200

    Add files via upload

---
+++ KeepPowerbankAlive.ino
@@ -0,0 +1,18 @@
+#define BLINK_GPIO 21 // define gpio
+unsigned long previousMillis;
+
+void loop()
+{
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= 5000) {
+        previousMillis = currentMillis;
+        digitalWrite(BLINK_GPIO, HIGH); 
+        delay(50);
+        digitalWrite(BLINK_GPIO, LOW);
+    }
+}
+
+void setup()
+{
+    pinMode(BLINK_GPIO, OUTPUT);
+}
